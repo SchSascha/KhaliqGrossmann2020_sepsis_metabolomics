b689e695c807f5a855eb63ddd3cac204d0701b93
PeterGrossmann <peter.grossmann@uni-jena.de>
2018-04-13 11:25:35 +0200

    UPDATE include Recon3D.

--- code/setup/extract_hmdb_metabolite_data.R
+++ code/setup/extract_hmdb_metabolite_data.R
@@ -35,19 +35,28 @@ human_sepsis_mets <- get_human_sepsis_legend()
 
 gsm_mets_list <- list()
 gsm_mets_list[["Recon2.04"]] <- fread(input = "../../data/template_models/Recon2.v04.mat_/recon2_met_names.txt", sep = "\n", header = FALSE, blank.lines.skip = TRUE)
-gsm_mets_list[["iHsa"]] <- fread(input = "../../data/template_models/iHsa_iRno/iHsa_met_names.txt", sep = "\n", header = FALSE)
+gsm_mets_list[["iHsa"]] <- fread(input = "../../data/template_models/iHsa_mets.txt", colClasses = "character")
+gsm_mets_list[["iRno"]] <- fread(input = "../../data/template_models/iRno_mets.txt", colClasses = "character")
+gsm_mets_list[["Recon3D"]] <- fread(input = "../../data/template_models/Recon3D_mets.txt", colClasses = "character")
+
+gsm_mets_list$Recon2.04 <- data.frame(Name = unlist(gsm_mets_list$Recon2.04))
+gsm_mets_list$Recon2.04[["CHEBI ID"]] <- ""
+gsm_mets_list$Recon2.04[["KEGG ID"]] <- ""
+gsm_mets_list$Recon2.04[["PUBCHEM ID"]] <- ""
+
+recon3D_hmdb_ids <- read.table(file = "../../data/template_models/Recon3D_HMDB_IDs.txt", blank.lines.skip = FALSE, sep = "\n", header = TRUE, stringsAsFactors = FALSE)
 
 gsm_S_list <- list()
-gsm_S_list[["Recon2.04"]] <- fread(input = "../../data/template_models/Recon2.v04.mat_/recon2_stoich_mat.txt", header = FALSE, sep = ",")
-gsm_S_list[["iHsa"]] <- fread(input = "../../data/template_models/iHsa_iRno/iHsa_stoich_mat.txt", header = FALSE, sep = "\n")
-gsm_S_list[["iHsa"]] <- unlist(lapply(gsm_S_list[["iHsa"]], function(x) stri_replace_all_fixed(str = x, pattern = ",", replacement = "\t"))) #stoich. mat. switches seperator inbetween ...
-gsm_S_list[["iHsa"]] <- fread(paste(gsm_S_list[["iHsa"]], sep = "\n", collapse = "\n"), header = FALSE, sep = "\t")
+gsm_S_list[["Recon2.04"]] <- fread(input = "../../data/template_models/Recon2.v04.mat_/recon2_stoich_mat.txt", header = FALSE, sep = ",", colClasses = "numeric")
+gsm_S_list[["iHsa"]] <- fread(input = "../../data/template_models/iHsa_stoich_mat.txt", header = FALSE, sep = "\t", colClasses = "numeric")
+gsm_S_list[["iRno"]] <- fread(input = "../../data/template_models/iRno_stoich_mat.txt", header = FALSE, sep = "\t", colClasses = "numeric")
+gsm_S_list[["Recon3D"]] <- fread(input = "../../data/template_models/Recon3D_stoich_mat.txt", header = FALSE, sep = "\t", colClasses = "numeric")
 
 met_present_in_hmdb <- list()
 met_present_in_hmdb[["Human Sepsis"]] <- match(human_sepsis_mets$name, mnames_s) 
 met_present_in_hmdb[["Human Sepsis"]][is.na(met_present_in_hmdb[["Human Sepsis"]])] <- match(human_sepsis_mets$group[is.na(met_present_in_hmdb[["Human Sepsis"]])], mnames_s)
 for (model in names(gsm_mets_list)){
-  mets <- gsm_mets_list[[model]]
+  mets <- unlist(gsm_mets_list[[model]][,1])
   met_present_in_hmdb[[model]] <- match(mets, mnames_s)
 }
 
@@ -59,6 +68,10 @@ human_sepsis_data <- human_sepsis_data[human_sepsis_data$`CAP / FP` != "-",]
 pheno_col_idx <- (which(colnames(human_sepsis_data) == "LCA") + 1):ncol(human_sepsis_data)
 write_set <- subset(human_sepsis_data, Day == 0, -c(1:3, 5, pheno_col_idx))
 write.csv(x = write_set, file = "metaboAnalyst_sepsis_temp.csv")
+
+write.csv(x = gsm_mets_list$iHsa$Name, file = "metaboAnalyst_iHsa_temp.csv")
+write.csv(x = gsm_mets_list$Recon2.04$Name, file = "metaboAnalyst_Recon2.04_temp.csv")
+write.csv(x = gsm_mets_list$Recon3D$Name, file = "metaboAnalyst_Recon3D_temp.csv")
 
 #Init
 rm("mSet", "mSet1", "mSet2")
@@ -118,13 +131,34 @@ mSet2<-CrossReferencing(mSet2, "name")
 mSet2<-CreateMappingResultTable(mSet2)
 
 #mapping consistency check
-match_hmdb_ids <- cbind(mSet1$dataSet$map.table[,2], mSet2$dataSet$map.table[,2])
-match_hmdb_ids[,1] <- match_hmdb_ids[match(mSet1$dataSet$map.table[,1], comp_i_list),1]
-match_hmdb_ids[,2] <- match_hmdb_ids[match(mSet2$dataSet$map.table[,1], comp_n_list),2]
-match_inconsist <- which(match_hmdb_ids[,1] != match_hmdb_ids[,2] & match_hmdb_ids[,1] != "NA" & match_hmdb_ids[,2] != "NA") #negligible
+match_hmdb_names <- cbind(mSet1$dataSet$map.table[,2], mSet2$dataSet$map.table[,2])
+match_hmdb_names[,1] <- match_hmdb_names[match(mSet1$dataSet$map.table[,1], comp_i_list),1] #metaboAnalyst mapping reorders metabolites
+match_hmdb_names[,2] <- match_hmdb_names[match(mSet2$dataSet$map.table[,1], comp_n_list),2] #metaboAnalyst mapping reorders metabolites
+match_inconsist <- which(match_hmdb_names[,1] != match_hmdb_names[,2] & match_hmdb_names[,1] != "NA" & match_hmdb_names[,2] != "NA") #negligible inconsistencies
 
-master_match_list <- c(match_hmdb_ids[1:82, 2], match_hmdb_ids[83:172, 1], match_hmdb_ids[173:203, 2])
-master_match_list[188] <- match_hmdb_ids[188,1]
+master_match_list <- c(match_hmdb_names[1:82, 2], match_hmdb_names[83:172, 1], match_hmdb_names[173:203, 2])
+master_match_list[188] <- match_hmdb_names[188,1]
+
+#mapping overlap for models and measured metabs
+match_hmdb_ids <- cbind(mSet1$dataSet$map.table[,3], mSet2$dataSet$map.table[,3])
+hmdb_id_match_list <- c(match_hmdb_ids[1:82, 2], match_hmdb_ids[83:172, 1], match_hmdb_ids[173:203, 2])
+hmdb_id_match_list[188] <- match_hmdb_ids[188,1]
+sum(hmdb_id_match_list %in% recon3D_hmdb_ids$HMDB.ID)
+
+match_kegg_ids <- cbind(mSet1$dataSet$map.table[,5], mSet2$dataSet$map.table[,5])
+kegg_id_match_list <- c(match_kegg_ids[1:82, 2], match_kegg_ids[83:172, 1], match_kegg_ids[173:203, 2])
+kegg_id_match_list[188] <- match_kegg_ids[188,1]
+sum(kegg_id_match_list %in% gsm_mets_list$Recon3D$`KEGG ID`) #all phosphatidylcholine like match to just one compound, hence the high match count
+
+match_pubchem_ids <- cbind(mSet1$dataSet$map.table[,4], mSet2$dataSet$map.table[,4])
+pubchem_id_match_list <- c(match_pubchem_ids[1:82, 2], match_pubchem_ids[83:172, 1], match_pubchem_ids[173:203, 2])
+pubchem_id_match_list[188] <- match_pubchem_ids[188,1]
+sum(pubchem_id_match_list %in% gsm_mets_list$Recon3D$`PUBCHEM ID`)
+
+hir <- hmdb_id_match_list %in% recon3D_hmdb_ids$HMDB.ID
+ker <- kegg_id_match_list %in% gsm_mets_list$Recon3D$`KEGG ID`
+pir <- pubchem_id_match_list %in% gsm_mets_list$Recon3D$`PUBCHEM ID`
+sum(hir | ker | pir)
 
 write_set2 <- write_set
 colnames(write_set2)[-1] <- master_match_list
@@ -139,13 +173,17 @@ mSetFinal<-CreateMappingResultTable(mSetFinal)
 map_table <- mSetFinal$dataSet$map.table
 map_table <- map_table[match(map_table[,1], master_match_list)]
 
-mSetFinal <- SetCurrentMsetLib(mSetFinal, "pathway", 0);
-#mSetFinal <- SetKEGG.PathLib(mSetFinal, "hsa")
-mSetFinal <- SetMetabolomeFilter(mSetFinal, F)
+#Init for GSMM
+rm("mSet_mod")
+mSet_mod <- InitDataObjects("conc", "list", FALSE)
+mSet_modlist <- list()
 
-mSetFinal <- CalculateOraScore(mSetFinal, "rbc", "hyperg")
-mSetFinal <- CalculateHyperScore(mSetFinal)
-#mSetFinal <- PlotORA(mSetFinal, "ora_0_", "bar", "png", 72, width=NA)
-#mSetFinal <- PlotPathSummary(mSetFinal, "path_view_0_", "png", 72, width=NA)
-mSetFinal <- CalculateGlobalTestScore(mSetFinal)
-mSetFinal <- PerformIntegPathwayAnalysis(mSetFinal, "dc", "hyper", "integ")
+#Clean relevant metabolite names
+
+#Match metabolite names to DB
+for (name in names(gsm_mets_list)){
+  mSet_modlist[[name]] <- mSet_mod
+  mSet_modlist[[name]] <- Setup.MapData(mSet_modlist[[name]], gsm_mets_list[[name]]$Name)
+  mSet_modlist[[name]] <- CrossReferencing(mSet_modlist[[name]], "name")
+  mSet_modlist[[name]] <- CreateMappingResultTable(mSet_modlist[[name]])
+}
