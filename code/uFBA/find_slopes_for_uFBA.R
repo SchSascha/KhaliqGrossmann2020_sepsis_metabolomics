b4497783a8c387bd594284673b6cbb0570a36f61
Peter <peter.grossmann@uni-jena.de>
2018-06-14 15:22:02 +0200

    More wip for uFBA

--- code/uFBA/find_slopes_for_uFBA.R
+++ code/uFBA/find_slopes_for_uFBA.R
@@ -1,22 +1,55 @@
 # Compute slopes and confidence intervals for rat metabolites
 
+library(matrixStats)
+
+source("../function_definitions.R")
+
+out_dir <- "../../data/uFBA/"
 
 # Read rat data
 
+rat_data <- get_rat_sepsis_data()
 
 # Remove outlier samples
 
+rat_data <- rat_data[!rat_data$`Sample Identification` %in% c("060H", "039L"), ]
 
-# Fit lm
+# Convert time point to numeric and change colname
+
+rat_data$`time point` <- as.numeric(sub(pattern = "h", replacement = "", x = rat_data$`time point`))
+colnames(rat_data)[colnames(rat_data) == "time point"] <- "time"
+
+# Add up pool concentrations
+
+# Fit lm for each metabolite and time change
 formula <- concentration ~ time
-
-## TODO: find elegant way to index the rat data; maybe unique?
-
-for (mat in c("liver", "heart")){
-	for (grp in unique(rat_sepsis_data$group)){
-		for (met in colnames(rat_sepsis_data)[metab_sel])){
-			subdat <- subset(rat_sepsis_data, group == grp & 
-			lm_list[[mat]][[met]] <- lm(formula, data = )
-
+lm_list <- list()
+matv <- c("liver", "heart", "plasma")
+grpv <- unique(rat_data$group)
+timev <- list(c(6, 24), c(24, 72))
+for (mat in matv){
+  lm_list[[mat]] <- list()
+	for (grp in grpv){
+	  lm_list[[mat]][[grp]] <- list()
+	  grpdat <- subset(rat_data, group == grp & material == mat)
+	  grpdat <- subset(grpdat, TRUE, which(!colAlls(is.na(grpdat))))
+	  tsv <- timev[sapply(timev, function(te) all(te %in% grpdat$time))]
+	  met_set <- colnames(grpdat)[5:ncol(grpdat)]
+	  for (time in tsv){
+	    tstr <- paste0(time, collapse = "_")
+	    lm_list[[mat]][[grp]][[tstr]] <- list()
+		  for (met in met_set){
+			  data <- subset(grpdat, grpdat$time %in% time, c("time", met))
+			  colnames(data)[2] <- "concentration"
+			  lm_list[[mat]][[grp]][[tstr]][[met]] <- lm(formula = formula, data = data)
+		  }
+		}
+  }
 }
+lm_summary <- lapply(lm_list, lapply, lapply, lapply, summary)
+lm_confint <- lapply(lm_list, lapply, lapply, lapply, confint)
+lm_rsqr <- unlist(lapply(lm_summary, lapply, lapply, lapply, function(r) r$r.squared))
+lm_slope <- lapply(lm_summary, lapply, lapply, lapply, function(s) try(s$coefficients[2, 1]))
+lm_cilen <- lapply(lm_confint, lapply, lapply, sapply, function(s) try(s[2, 1]))
 
+# Write results to disk
--- code/uFBA/run_uFBA.m
+++ code/uFBA/run_uFBA.m
@@ -3,6 +3,13 @@
 % author: Peter Grossmann
 % date: 14.06.2018 (initial version)
 
+out_dir = '../../results/uFBA/';
+
+% Prepare out dir
+if (exist(out_dir, 'dir') == 0)
+    mkdir(out_dir);
+end
+
 % Init COBRA toolbox
 cobra_dir = '../cobratoolbox/';
 code_dir = cd(cobra_dir);
@@ -11,14 +18,14 @@ changeCobraSolver('gurobi', 'ALL');
 cd(code_dir);
 
 % Read metabolic template model
-iRno = load('../../data/template_models/iRno.mat');
-model = iRno.model;
+load('../../data/template_models/iRno.mat');
+model = iRno;
 
 % Collect input files to run uFBA on
 in_files = ls('../../data/uFBA/');
 in_files = strsplit(in_files, '\n|\w');
 
-uFBAmodels = cell();
+uFBAmodels = cell(length(in_files));
 
 % Run uFBA on input files
 for n = 1:length(in_files) 
@@ -27,8 +34,35 @@ for n = 1:length(in_files)
     vars.changeSlopes = data.slope;
     vars.changeIntervals = data.confint;
     um = buildUFBAmodel(model, vars);
-    uFBAmodels{n} = um;
+    uFBAmodels{n} = um; % The target fields are f (obj) and v (rxn fluxes)
 end
 
 % Save built models
-save('../../results/uFBA/models.mat', 'uFBAmodels');
+save(strcat(out_dir, 'models.mat'), 'uFBAmodels');
+
+%% As example:
+
+cobra_dir = '../cobratoolbox/';
+code_dir = cd(cobra_dir);
+initCobraToolbox;
+changeCobraSolver('gurobi', 'ALL');
+cd(code_dir);
+tutorialPath = fileparts(which('tutorial_uFBA.mlx'));
+load([tutorialPath filesep 'sample_data.mat']);
+model = readCbModel([tutorialPath filesep 'sample_data.mat'],'modelName','model');
+changeSlopes = zeros(length(met_IDs), 1);
+changeIntervals = zeros(length(met_IDs), 1);
+for i = 1:length(met_IDs)
+    [tmp1, tmp2] = regress(met_data(:, i), [time ones(length(time), 1)], 0.05);
+    changeSlopes(i, 1) = tmp1(1);
+    changeIntervals(i, 1) = abs(changeSlopes(i, 1) - tmp2(1));
+end
+tmp1 = changeSlopes - changeIntervals;
+tmp2 = changeSlopes + changeIntervals;
+ignoreSlopes = double(tmp1 < 0 & tmp2 > 0);
+uFBAvariables.metNames = met_IDs;
+uFBAvariables.changeSlopes = changeSlopes;
+uFBAvariables.changeIntervals = changeIntervals;
+uFBAvariables.ignoreSlopes = ignoreSlopes;
+uFBAoutput = buildUFBAmodel(model, uFBAvariables);
+model_ufba = optimizeCbModel(uFBAoutput.model);
